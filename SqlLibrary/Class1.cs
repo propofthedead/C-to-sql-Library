3a00bd6d68f361686757bdade1715f2266cb964f
Foster Stule <camodude5@yahoo.com>
2018-06-07 09:45:38 -0400

    created a class library

---
+++ SqlLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLibrary
+{
+    public class Class1
+    {
+    }
+}
